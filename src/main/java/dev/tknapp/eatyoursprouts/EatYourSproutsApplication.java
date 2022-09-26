5c3c76e37922a6bc77f85efc515b27381b13a559
Tyler Knapp <tylerknapp25@gmail.com>
2022-09-26 11:10:36 -0400

    initial commit

---
+++ src/main/java/dev/tknapp/eatyoursprouts/EatYourSproutsApplication.java
@@ -0,0 +1,13 @@
+package dev.tknapp.eatyoursprouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatYourSproutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatYourSproutsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tknapp/eatyoursprouts/EatYourSproutsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tknapp.eatyoursprouts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatYourSproutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
